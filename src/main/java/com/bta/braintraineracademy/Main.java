08a188a92d1f23a7117eb6933b6c83d6a47cb15e
mdajaz <ajazansari11102@gmail.com>
2024-09-28 12:23:18 +0530

    first time commit

---
+++ src/main/java/com/bta/braintraineracademy/Main.java
@@ -0,0 +1,9 @@
+package com.bta.braintraineracademy;
+
+public class Main {
+    static int i = 10;
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(i);
+    }
+}
