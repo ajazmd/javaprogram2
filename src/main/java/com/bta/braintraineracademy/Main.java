c2229ea41f4ebfccd578d15db7ffee675f064e46
mdajaz <ajazansari11102@gmail.com>
2024-09-28 12:33:18 +0530

    second time commit

--- src/main/java/com/bta/braintraineracademy/Main.java
+++ src/main/java/com/bta/braintraineracademy/Main.java
@@ -2,8 +2,9 @@ package com.bta.braintraineracademy;
 
 public class Main {
     static int i = 10;
+    static int j= 12 ;
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println(i);
+        System.out.println(i+j);
     }
 }
